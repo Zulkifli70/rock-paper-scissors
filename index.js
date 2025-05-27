659c985bac871a8baf18d448b8edc8b1a7f6944b
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-05-27 08:58:42 +0700

    Make the rules of the game
    Create 2 variable that will keep track the player score, Make playRound function to make the game rules of rock paper scissor game, Make playGame function to make the game length that is 5 round and make If statement that declares the winner

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Make a rock paper scissor game
+// Make Computer choice that will choose between rock paper and scissor
+// Make human choice using prompt
+// Create 2 variable that will keep track the player score
+// Make playRound function to make the game rules of rock paper scissor game
+// Make playGame function to make the game length that is 5 round and make If statement that declares the winner
+
 function getComputerChoice() {
   let random = Math.random();
 
@@ -10,18 +17,48 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice(`Computer Choice is ${getComputerChoice}`));
-
-let input = window.prompt("Tentukan Pilihanmu !");
-
 function getHumanChoice() {
-  if (input === "rock") {
-    return "rock";
-  } else if (input === "paper") {
-    return "paper";
-  } else if (input === "scissor") {
-    return "scissor";
+  let input = prompt("Choose Wisely my man.. (Rock, Paper, Scissor)");
+  return input.toLowerCase();
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log(`Its a tie!!!, You both using ${humanChoice}`);
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissor") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissor" && computerChoice === "paper")
+    ) {
+      console.log(`You Win!!!!!, ${humanChoice} win against ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose!!, ${computerChoice} win against ${humanChoice} `);
+      computerScore++;
+    }
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+
+  console.log(
+    `Your Final Score is: You:${humanScore}, Computer:${computerScore}`
+  );
+
+  if (humanScore > computerScore) {
+    console.log("Congrats, You win!!");
+  } else if (computerScore > humanScore) {
+    console.log("You Are Cooked mate !!");
+  } else {
+    console.log("its a draw mate, Cheers");
   }
 }
 
-console.log(getHumanChoice(`Your Choice is ${input}`));
+playGame();
